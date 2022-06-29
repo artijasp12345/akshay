5918f820079189cbf7898530ee0ad274792118fe
artijasp12345 <105384483+artijasp12345@users.noreply.github.com>
2022-06-29 23:49:16 +0530

    Update Demo1Application.java
    commit1

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//this is 1st project
+
 @SpringBootApplication
 public class Demo1Application {
 
